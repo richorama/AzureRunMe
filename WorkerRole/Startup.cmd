b45ce71e7ea8890b81b18a13ee28d3600afbc06a
Rob Blackwell <rob.blackwell@aws.net>
2011-07-01 14:06:19 +0100

    Security - rem out firewall disable, install to c:\applications not c:\

--- WorkerRole/Startup.cmd
+++ WorkerRole/Startup.cmd
@@ -1,10 +1,11 @@
 REM I'm running at startup with elevated privileges
 time /t > starting.txt
-cacls c:\ /t /e /c /g everyone:f
+mkdir c:\applications
+cacls c:\applications /t /e /c /g everyone:f
 if exist started.txt goto skip
-REM Run this section only once
+rem Run this section only once
 rem ServerManagerCmd.exe -install SMTP-Server -restart
-netsh advfirewall set allprofiles state off
+rem netsh advfirewall set allprofiles state off
 :skip
 time /t > started.txt
 exit /b 0
