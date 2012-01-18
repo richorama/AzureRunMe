79989185fe38282589e3e625ce6d554460484a79
Rob Blackwell <rob.blackwell@two10degrees.com>
2012-01-18 14:23:59 +0100

    Reboot from the portal wipes the deployment, but not the registry so mov the control files to c:\applications to avoid a double reboot - thanks Ken.

--- WorkerRole/Startup.cmd
+++ WorkerRole/Startup.cmd
@@ -2,10 +2,10 @@ REM I'm running at startup with elevated privileges
 time /t > starting.txt
 mkdir c:\applications
 cacls c:\applications /t /e /c /g everyone:f
-if exist started.txt goto skip
+if exist c:\applications\started.txt goto skip
 rem Run this section only once
 rem ServerManagerCmd.exe -install SMTP-Server -restart
 rem netsh advfirewall set allprofiles state off
 :skip
-time /t > started.txt
+time /t > c:\applications\started.txt
 exit /b 0
